e73a3e0fc77e4bdb0d5b6a3dfb642f7a3a3d9cfe
Kipsome <kiplimotevin@gmail.com>
2022-03-14 10:03:12 -0700

    My commit message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*
+*main - Entry point
+*My hello, printf task
+*Return:return 1 after program excecution
+*/
+int main(void)
+{
+printf("and that piece of art is useful,\n");
+return (1) ;
+}
