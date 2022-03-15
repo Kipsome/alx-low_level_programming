5eb7a35ce5f5a5997b84ce8d78f60fff8d665792
kip <kiplimotevin@gmail.com>
2022-03-15 19:36:13 +0300

    My commit message

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include 'holberon.h"[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C"holberton.h[D[D[D[D[D[D[D[D[D[D[D[D "holberton.h"
+/**
+  *print_alphabet_x10 - repeats the prinbt_alphabet 10 times.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[[C[C[C[C[C[C[C[C[C[C[Ct_alphabet 10 times;
+  *Return: Nothing;
+  */
+void print_alphabet_x10(void)
+{
+     int i;
+     char c;
+
+     for (i - 0; i < 10; ++i)
+     {
+             for (c = 'a'; c <= 'z'; c++)
+             {
+                      _putchar(c);
+             }[A[A[B[B
+             _putchar('\n');
+     }
+}
