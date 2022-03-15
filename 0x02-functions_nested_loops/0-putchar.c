cdb6dd5176fdd0dfacf7c1dc6b3874e3fe27dfe0
kip <kiplimotevin@gmail.com>
2022-03-15 16:07:08 +0300

    My commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,4 @@
+#include <stdio>
+
+inr main()[A[A[C[C[C[C[C[B[B[B[D[D[D[D[D
+[A[C[C[t[C[C[C[C[C[C[C[B[D[D[D[D[D[D[D[D[D[D
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My readme file for 0x02-functios_nested_loops
